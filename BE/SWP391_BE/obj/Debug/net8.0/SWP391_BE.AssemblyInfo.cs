d9af99de2cbb42115a54f086f697d06640dd4b25
Anlq <lequangan2404@gmail.com>
2025-03-22 00:57:58 +0700

    update

--- BE/SWP391_BE/obj/Debug/net8.0/SWP391_BE.AssemblyInfo.cs
+++ BE/SWP391_BE/obj/Debug/net8.0/SWP391_BE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP391_BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c3391a3d9cc7c3d06dbcfe5452634fa4e7f28d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac12eb9213f414416f4931d7cc44feabb67f7f4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP391_BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP391_BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
