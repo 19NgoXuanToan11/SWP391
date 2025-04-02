5233f973dc38ce4e647d580486463ebc86d660ad
Tôn Thất Trung Hiếu <trunghieutonthat@gmail.com>
2025-04-03 00:48:46 +0700

    update stock

--- BE/Service/IProductService.cs
+++ BE/Service/IProductService.cs
@@ -13,5 +13,7 @@ namespace Service
         Task UpdateProductAsync(Product product);
         Task DeleteProductAsync(int id);
         Task<IEnumerable<Product>> SearchProductsAsync(string searchTerm);
+        Task UpdateProductStockAsync(int productId, int quantityToReduce);
+        Task RestoreProductStockAsync(int productId, int quantityToRestore);
     }
 }
